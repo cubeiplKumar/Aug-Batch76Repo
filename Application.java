30e41d9d3b14e19989373fed778b52d438dfc1b5
cubeiplkumar <emailanilkumarm@gmail.com>
2023-08-15 08:13:01 +0530

    feature302 changes

--- Application.java
+++ Application.java
@@ -24,4 +24,24 @@
             Return_Value obj = new Return_Value();
             obj.input();
         }
+		#feature302  changes by Keerthi...Login Module
+		public class Object_Pass_Return 
+{
+    int length, breadth, area;
+    Object_Pass_Return area1(Object_Pass_Return object1)
+    {
+        object1 = new Object_Pass_Return();
+        object1.length = this.length;
+        object1.breadth = this.breadth;
+        object1.area = object1.length * object1.breadth;
+        return object1;
     }
+    Object_Pass_Return area2(Object_Pass_Return object2)
+    {
+        object2 = new Object_Pass_Return();
+        object2.length = this.length + 5;
+        object2.breadth = this.breadth + 6;
+        object2.area = object2.length * object2.breadth;
+        return object2;
+    }
+		    }
